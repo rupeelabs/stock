5c216404e67e7058ffedc39ef31a8e6979408c51
eric.wang <eric.wang@gaodun.com>
2019-01-11 20:05:10 +0800

    x

--- app/Service/SpiderService.php
+++ app/Service/SpiderService.php
@@ -57,9 +57,8 @@ class SpiderService
     {
         $stocks = \DB::select("select * from stock");
         foreach ($stocks as $stock) {
-            $code =  '000001';
+            $code =  $stock->code;
             $url = "http://pdfm.eastmoney.com/EM_UBG_PDTI_Fast/api/js?token=4f1862fc3b5e77c150a2b985b12db0fd&rtntype=6&id={$code}{$stock->market_type}&type=k&authorityType=fa&cb=jsonp1546755196396";
-            $url = "http://pdfm.eastmoney.com/EM_UBG_PDTI_Fast/api/js?token=4f1862fc3b5e77c150a2b985b12db0fd&rtntype=6&id=0000012&type=k&authorityType=fa&cb=jsonp1547206180243";
             $stock = json_decode($this->grab($url), true);
             $flows = $stock['data'];
 
