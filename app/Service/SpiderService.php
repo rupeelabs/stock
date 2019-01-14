6963f91b9cee33b991c95652ca1541cfbe811b46
eric.wang <eric.wang@gaodun.com>
2019-01-14 18:26:05 +0800

    bug fixed

--- app/Service/SpiderService.php
+++ app/Service/SpiderService.php
@@ -66,19 +66,15 @@ class SpiderService
             $url = "http://pdfm.eastmoney.com/EM_UBG_PDTI_Fast/api/js?token=4f1862fc3b5e77c150a2b985b12db0fd&rtntype=6&id={$code}{$stock->market_type}&type=k&authorityType=fa&cb=jsonp1546755196396";
             $stock = json_decode($this->grab($url), true);
             $flows = $stock['data'];
+            if (empty($flows))
+                continue;
             if ($isAll == 'no') {
                 $temp = [];
                 $temp[] = last($flows);
                 $flows = $temp;
             }
             foreach ($flows as $flow) {
-                try {
-                    list($date, $open, $close, $highest, $lowest, $vol, $turnover, $amplitude) = explode(',', $flow);
-                } catch (\Exception $e) {
-                    echo $e->getMessage();
-                    echo $code;
-                    echo $flow;exit;
-                }
+                list($date, $open, $close, $highest, $lowest, $vol, $turnover, $amplitude) = explode(',', $flow);
                 $result = \DB::select(sprintf(
                     "select id from stock_flow where code='%s' and date='%s'",
                     $code, $date
