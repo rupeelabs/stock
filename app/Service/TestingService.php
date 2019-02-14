dfe15f19170b2e49211462831930126f12187f76
eric.wang <eric.wang@gaodun.com>
2019-02-14 21:19:16 +0800

    bug fixed

--- app/Service/TestingService.php
+++ app/Service/TestingService.php
@@ -87,7 +87,7 @@ value(?,?)",
                     $flow->close > $flow->open &&
 //                    (($flow->highest/$flow->lowest - 1)*100 >8) &&
 //                    (($flow->close/$flow->open - 1)*100 <0.3) &&
-                    (($flow->open/$flow->lowest - 1)*100 >11)
+                    (($flow->open/$flow->lowest - 1)*100 >10)
                 ) {
                     if (\DB::select(sprintf(
                         "select id from `cross` where code='%s' and date='%s'",
