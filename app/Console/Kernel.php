dba63daabd2988d65531c5614f2ca11e78f683e8
eric.wang <eric.wang@gaodun.com>
2019-04-15 10:51:21 +0800

    3

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -81,7 +81,7 @@ class Kernel extends ConsoleKernel
         $schedule->command(ZHIBIAO::class)->cron('30 11 * * *');
         $schedule->command(GAT::class)->cron('10 19 * * *');
         $schedule->command(VRT::class)->cron('22 19 * * *');
-        $schedule->command(ZLZJ::class)->cron('*/2 * * * *');
+        $schedule->command(ZLZJ::class)->cron('*/3 * * * *');
 
 
         $schedule->command(ThirdReminder::class)->cron('10 10 * * *');
